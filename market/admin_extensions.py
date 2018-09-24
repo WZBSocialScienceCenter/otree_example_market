09e424d2e0efe0c4229521e0838cb6837699f63c
Markus Konrad <markus.konrad@wzb.eu>
2018-09-24 15:06:48 +0200

    session data view works

--- market/admin_extensions.py
+++ market/admin_extensions.py
@@ -2,15 +2,22 @@ from collections import OrderedDict, defaultdict
 from importlib import import_module
 from pprint import pprint
 
-from otree.views.admin import SessionData, AdminSessionPageMixin, pretty_name, pretty_round_name
+from otree.views.admin import SessionData, pretty_name, pretty_round_name
 from otree.export import sanitize_for_live_update, get_rows_for_live_update
 from otree.db.models import Model
 
 
 class SessionDataExtension(SessionData):
     @staticmethod
-    def get_field_names_for_custom_model(model):
-        return [f.name for f in model._meta.fields]
+    def get_field_names_for_custom_model(model, conf):
+        if 'fields' in conf:
+            fields = conf['fields']
+        else:
+            fields = [f.name for f in model._meta.fields]
+
+        exclude = set(conf.get('exclude_fields', []))
+
+        return [f for f in fields if f not in exclude]
 
     @staticmethod
     def get_custom_models_conf(models_module):
@@ -57,17 +64,76 @@ class SessionDataExtension(SessionData):
                     row = []
                     for colname in colnames:
                         attr = getattr(res, colname, '')
+                        if isinstance(attr, Model):
+                            attr = attr.pk
+                        
                         row.append(sanitize_for_live_update(attr))
 
                     rows[key][model_name_lwr].append(row)
 
         return rows
 
-    def custom_columns_builder(self, custom_model_classes):
-        columns_for_models = {model.__name__.lower(): self.get_field_names_for_custom_model(model)
-                              for model in custom_model_classes}
+    def custom_columns_builder(self, custom_model_conf):
+        columns_for_models = {name.lower(): self.get_field_names_for_custom_model(conf['class'], conf['data_view'])
+                              for name, conf in custom_model_conf.items()}
 
         return columns_for_models
+
+    @staticmethod
+    def combine_rows(subsession_rows, custom_rows, columns_for_models, columns_for_custom_models,
+                     custom_models_names_lwr, custom_models_baseclass, custom_models_basekey):
+        combined_rows = []
+        otree_modelnames = ['Player', 'Group', 'Subsession']
+        otree_modelnames_lwr = [n.lower() for n in otree_modelnames]
+        for row in subsession_rows:
+            # find row key
+            row_key = None
+
+            i = 0
+            for ot_model_name in otree_modelnames:
+                model_fields = columns_for_models[ot_model_name.lower()]
+                n_fields = len(model_fields)
+
+                if ot_model_name == custom_models_baseclass:
+                    try:
+                        key_idx = model_fields.index(custom_models_basekey)
+                        row_key = row[i + key_idx]
+                    except ValueError:  # custom_models_basekey not in model_fields
+                        pass
+
+                i += n_fields
+
+            if row_key is None:
+                raise ValueError('no row key found (base class: `%s`, key name: `%s`)'
+                                 % (custom_models_baseclass, custom_models_basekey))
+
+            combrow = []  # combined row
+            n_fields_from_otree_models = 0
+            for model_name in ['player'] + custom_models_names_lwr + ['group', 'subsession']:
+                if model_name in otree_modelnames_lwr:
+                    n_fields = len(columns_for_models[model_name])
+                    field_idx_start = n_fields_from_otree_models
+                    field_idx_end = n_fields_from_otree_models + n_fields
+                    combrow.extend(row[field_idx_start:field_idx_end])
+                    n_fields_from_otree_models += n_fields
+                else:
+                    n_fields = len(columns_for_custom_models[model_name])
+                    custom_vals = custom_rows[row_key][model_name]
+                    if custom_vals:
+                        assert all(n == n_fields for n in map(len, custom_vals))
+                        for col_values in zip(*custom_rows[row_key][model_name]):
+                            col_values = [v if v else '&nbsp;' for v in col_values]
+                            combrow.append('<hr>'.join(col_values))
+                    else:
+                        combrow.extend([''] * n_fields)
+
+            combined_rows.append(combrow)
+
+        n_cols_otree_models = sum(map(len, columns_for_models.values()))
+        n_cols_custom_models = sum(map(len, columns_for_custom_models.values()))
+        assert all(n == n_cols_otree_models + n_cols_custom_models for n in map(len, combined_rows))
+
+        return combined_rows
 
     def get_context_data(self, **kwargs):
         def columns_for_any_modelname(modelname):
@@ -92,72 +158,24 @@ class SessionDataExtension(SessionData):
             custom_models_conf = self.get_custom_models_conf(models_module)
             custom_models_names = list(custom_models_conf.keys())
             custom_models_names_lwr = [n.lower() for n in custom_models_names]
-            custom_models_classes = [conf['class'] for conf in custom_models_conf.values()]
 
             # can't use subsession._meta.app_config.name, because it won't work
             # if the app is removed from SESSION_CONFIGS after the session is
             # created.
             columns_for_models, subsession_rows = get_rows_for_live_update(subsession)
 
-            columns_for_custom_models = self.custom_columns_builder(custom_models_classes)
+            columns_for_custom_models = self.custom_columns_builder(custom_models_conf)
             custom_models_qs, custom_models_baseclass, custom_models_basekey = \
                 self.custom_rows_queryset(models_module, custom_models_names, subsession=subsession)
             custom_rows = self.custom_rows_builder(custom_models_qs, columns_for_custom_models,
                                                    custom_models_baseclass, custom_models_basekey)
-            pprint(subsession_rows)
-            pprint(columns_for_custom_models)
-            pprint(custom_rows)
+            # pprint(subsession_rows)
+            # pprint(columns_for_custom_models)
+            # pprint(custom_rows)
 
-            combined_rows = []
-            otree_modelnames = ['Player', 'Group', 'Subsession']
-            otree_modelnames_lwr = [n.lower() for n in otree_modelnames]
-            for row in subsession_rows:
-                # find row key
-                row_key = None
-
-                i = 0
-                for ot_model_name in otree_modelnames:
-                    model_fields = columns_for_any_modelname(ot_model_name.lower())
-                    n_fields = len(model_fields)
-
-                    if ot_model_name == custom_models_baseclass:
-                        try:
-                            key_idx = model_fields.index(custom_models_basekey)
-                            row_key = row[i + key_idx]
-                        except ValueError:   # custom_models_basekey not in model_fields
-                            pass
-
-                    i += n_fields
-
-                if row_key is None:
-                    raise ValueError('no row key found (base class: `%s`, key name: `%s`)'
-                                     % (custom_models_baseclass, custom_models_basekey))
-
-                combrow = []  # combined row
-                n_fields_from_otree_models = 0
-                for model_name in ['player'] + custom_models_names_lwr + ['group', 'subsession']:
-                    n_fields = len(columns_for_any_modelname(model_name))
-
-                    if model_name in otree_modelnames_lwr:
-                        field_idx_start = n_fields_from_otree_models
-                        field_idx_end = n_fields_from_otree_models + n_fields
-                        combrow.extend(row[field_idx_start:field_idx_end])
-                        n_fields_from_otree_models += n_fields
-                    else:
-                        custom_vals = custom_rows[row_key][model_name]
-                        if custom_vals:
-                            assert all(n == n_fields for n in map(len, custom_vals))
-                            for col_values in zip(*custom_rows[row_key][model_name]):
-                                combrow.append(','.join(col_values))
-                        else:
-                            combrow.extend([''] * n_fields)
-
-                combined_rows.append(combrow)
-
-            pprint(combined_rows)
-            n_cols_otree_models = sum(map(len, columns_for_models.values()))
-            n_cols_custom_models = sum(map(len, columns_for_custom_models.values()))
-            assert all(n == n_cols_otree_models + n_cols_custom_models for n in map(len, combined_rows))
+            combined_rows = self.combine_rows(subsession_rows, custom_rows, columns_for_models,
+                                              columns_for_custom_models, custom_models_names_lwr,
+                                              custom_models_baseclass, custom_models_basekey)
 
             if not rows:
                 rows = combined_rows
@@ -212,5 +230,5 @@ class SessionDataExtension(SessionData):
         return context
 
     def get_template_names(self):
-        return ['otree/admin/SessionData.html']
+        return ['otree/admin/SessionDataExtension.html']
 
